bed9d89fd3c3e51e9a04886d15287fed5470770f
yerin <hepari2154@naver.com>
2019-02-21 01:59:22 +0900

    project init

---
+++ src/main/java/com/example/springsecurityguide/SpringsecurityguideApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityguideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityguideApplication.class, args);
+    }
+
+}
