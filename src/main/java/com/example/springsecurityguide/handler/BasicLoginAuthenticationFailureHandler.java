c73aa5313cf9baa22c0ca3c41c6ea4fb713d0d9b
yerin <hepari2154@naver.com>
2019-02-23 03:57:23 +0900

    Add 기본 인증(유저 아이디와 비밀번호로 로그인)에 대한 AuthenticationFailureHandler 작성

---
+++ src/main/java/com/example/springsecurityguide/handler/BasicLoginAuthenticationFailureHandler.java
@@ -0,0 +1,29 @@
+package com.example.springsecurityguide.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BasicLoginAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(exception.getMessage());
+
+    }
+}
