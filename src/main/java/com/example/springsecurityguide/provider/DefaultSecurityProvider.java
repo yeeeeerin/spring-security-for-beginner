61f4e6ebc8d423b79360af52af47107fe55391db
yerin <hepari2154@naver.com>
2019-02-23 01:57:49 +0900

    Add provide인증 구현

---
+++ src/main/java/com/example/springsecurityguide/provider/DefaultSecurityProvider.java
@@ -0,0 +1,43 @@
+package com.example.springsecurityguide.provider;
+
+
+import com.example.springsecurityguide.Service.MemberService;
+import com.example.springsecurityguide.domain.SecurityMember;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class DefaultSecurityProvider implements AuthenticationProvider {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = (String) authentication.getPrincipal();
+        String password = (String) authentication.getCredentials();
+
+        SecurityMember member = (SecurityMember) memberService.loadUserByUsername(email);
+
+        if (member == null) {
+            throw new BadCredentialsException("회원이 등록되어 있지 않습니다.");
+        }
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            throw new BadCredentialsException("비밀번호가 옳지 않습니다.");
+        }
+
+        return new UsernamePasswordAuthenticationToken(member, password, member.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
