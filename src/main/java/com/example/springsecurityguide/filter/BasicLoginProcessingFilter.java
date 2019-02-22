eaca8e83e41ae903ca10f324ad48d47ad6f33f6d
yerin <hepari2154@naver.com>
2019-02-23 04:02:01 +0900

    Add BasicLoginProcessingFilter 구현

--- src/main/java/com/example/springsecurityguide/filter/BasicLoginProcessingFilter.java
+++ src/main/java/com/example/springsecurityguide/filter/BasicLoginProcessingFilter.java
@@ -1,15 +1,18 @@
 package com.example.springsecurityguide.filter;
 
 import com.example.springsecurityguide.dto.LoginMemberDto;
+import com.example.springsecurityguide.handler.BasicLoginAuthenticationFailureHandler;
 import com.example.springsecurityguide.handler.BasicLoginAuthenticationSuccessHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +23,9 @@ public class BasicLoginProcessingFilter extends AbstractAuthenticationProcessing
 
     @Autowired
     BasicLoginAuthenticationSuccessHandler successHandler;
+
+    @Autowired
+    BasicLoginAuthenticationFailureHandler failureHandler;
 
     /*
     * todo urlmatcher랑 string 두개의 형식으로 생성자 생성하는거 설명하기
@@ -36,5 +42,16 @@ public class BasicLoginProcessingFilter extends AbstractAuthenticationProcessing
                 new UsernamePasswordAuthenticationToken(loginMemberDto.getEmail(),loginMemberDto.getPassword(), Collections.emptyList());
 
         return this.getAuthenticationManager().authenticate(token);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        successHandler.onAuthenticationSuccess(request, response, authResult);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        SecurityContextHolder.clearContext();
+        failureHandler.onAuthenticationFailure(request, response, failed);
     }
 }
