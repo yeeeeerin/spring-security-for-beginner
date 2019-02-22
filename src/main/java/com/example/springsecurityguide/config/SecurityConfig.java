1f2f25a0eb328839b01e2a0a0a4feaf6f3e3c05a
yerin <hepari2154@naver.com>
2019-02-23 04:16:27 +0900

    Add securityconfig 파일에 기본 로그인에 대한filter,provider등록과 successHandler 회원 정보 가져오는 부분 수정

--- src/main/java/com/example/springsecurityguide/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurityguide/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.example.springsecurityguide.config;
 
+import com.example.springsecurityguide.filter.BasicLoginProcessingFilter;
+import com.example.springsecurityguide.provider.BasicLoginSecurityProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +17,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    BasicLoginSecurityProvider basicLoginSecurityProvider;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -29,6 +36,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/h2-console/**").permitAll();
+    }
+
+    @Bean
+    protected BasicLoginProcessingFilter basicLoginProcessingFilter() throws Exception {
+        BasicLoginProcessingFilter filter = new BasicLoginProcessingFilter("/login");
+        filter.setAuthenticationManager(super.authenticationManagerBean());
+        return filter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth
+                .authenticationProvider(this.basicLoginSecurityProvider);
     }
 
 
