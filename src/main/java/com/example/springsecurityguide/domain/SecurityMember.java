9cfb85f8627b1246bdffcc9bf9f864834c7b1a1c
yerin <hepari2154@naver.com>
2019-02-26 03:51:45 +0900

    Add token claims 얻어오기

--- src/main/java/com/example/springsecurityguide/domain/SecurityMember.java
+++ src/main/java/com/example/springsecurityguide/domain/SecurityMember.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.User;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 
@@ -25,5 +26,19 @@ public class SecurityMember extends User {
         return Arrays.asList(role).stream()
                 .map(r -> new SimpleGrantedAuthority(r.getRoleName()))
                 .collect(Collectors.toList());
+    }
+
+    public static List<SimpleGrantedAuthority> parseAuthorities(String role) {
+
+        MemberRole memberRole = Arrays.stream(MemberRole.values())
+                .filter(r -> r.getRoleName().equalsIgnoreCase(role))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("권한이 없습니다"));
+
+        return parseAuthorities(memberRole);
+    }
+
+    public String getRole(){
+        return getAuthorities().stream().findFirst().get().getAuthority();
     }
 }
--- src/main/java/com/example/springsecurityguide/utils/JwtFactory.java
+++ src/main/java/com/example/springsecurityguide/utils/JwtFactory.java
@@ -1,11 +1,17 @@
 package com.example.springsecurityguide.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.Verification;
 import com.example.springsecurityguide.domain.SecurityMember;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 
 @Slf4j
@@ -24,6 +30,7 @@ public class JwtFactory {
         token = JWT.create()
                 .withIssuer(jwtSettings.getTokenIssuer())
                 .withClaim("EMAIL", securityMember.getUsername())
+                .withClaim("ROLE",securityMember.getRole())
                 .sign(Algorithm.HMAC256(jwtSettings.getTokenSigningKey()));
 
         log.info("token -- "+token);
@@ -31,5 +38,29 @@ public class JwtFactory {
         return token;
 
     }
+
+
+
+
+    public SecurityMember decodeToken(String token){
+
+        Verification verification = JWT.require(Algorithm.HMAC256(jwtSettings.getTokenSigningKey()));
+        JWTVerifier verifier = verification.build();
+        DecodedJWT decodedJWT;
+        try{
+            decodedJWT = verifier.verify(token);//验证token
+        }catch (Exception e){
+            return null;
+        }
+
+        Map<String, Claim> claims = decodedJWT.getClaims();
+
+        return new SecurityMember(
+                claims.get("EMAIL").asString(),
+                "1234",
+                SecurityMember.parseAuthorities(claims.get("ROLE").asString())
+        );
+    }
+
 
 }
